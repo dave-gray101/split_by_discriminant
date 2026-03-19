4f457a4e3b3805acfd317b7d893458e0991a59b8
Dave Lee <dave@gray101.com>
2026-03-19 16:01:41 -0400

    add secondary macro for testing
    Signed-off-by: Dave Lee <dave@gray101.com>

--- split_by_discriminant_macros/src/lib.rs
+++ split_by_discriminant_macros/src/lib.rs
@@ -3,6 +3,11 @@ use proc_macro::TokenStream;
 use split_by_discriminant::proc_macro::derive_extract_from;
 
 #[proc_macro_derive(ExtractFrom, attributes(extract_from))]
+pub fn d_extract_from(input: TokenStream) -> TokenStream {
+    derive_extract_from(input.into()).into()
+}
+
+#[proc_macro]
 pub fn extract_from(input: TokenStream) -> TokenStream {
     derive_extract_from(input.into()).into()
 }
